ec54372799a7330c0d2a2bad9d4bd68643708a46
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-22 19:21:03 +1030

    Create file

---
+++ 650-2KeysKeyboard/650-2KeysKeyboard.py
@@ -0,0 +1,19 @@
+class Solution:
+    def minSteps(self, n: int) -> int:
+        ans = 0
+
+        if n == 1:
+            return 0
+        
+        i = 2
+        while i * i <= n:
+            while n % i == 0:
+                ans += i
+                n //= i
+            i += 1
+
+
+        if n > 1:
+            ans += n
+        
+        return ans
