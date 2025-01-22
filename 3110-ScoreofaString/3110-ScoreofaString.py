b3bd6d5b429f89ca3525087d4fedc04aba19c877
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-22 19:36:20 +1030

    Create file

---
+++ 3110-ScoreofaString/3110-ScoreofaString.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def scoreOfString(self, s):
+        i = 0
+        # Score sum
+        sSum = 0
+        # Less then len(s) - 1 as we want to stop at second last index
+        while i < (len(s) -1):
+            # Adds absolute difference 
+            sSum += abs(ord(s[i]) - ord(s[i+1]))
+            i += 1
+        return sSum
