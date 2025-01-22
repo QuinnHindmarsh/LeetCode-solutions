5f0e37fdeb820636da50f972900e0cf0f982c149
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-22 19:16:30 +1030

    Update file 22/01/2025, 19:16:30

--- 67-AddBinary/67-AddBinary.py
+++ 67-AddBinary/67-AddBinary.py
@@ -1,3 +1,31 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        return bin(int(a,base=2) + int(b, base=2))[2::]
+        if len(a) < len(b):
+            a,b = b,a
+
+        flag = False
+        ans = ""
+        i = 0
+
+        while i < len(a):
+            numOnes = int(flag) + int(a[(-1-i)])
+            if i < len(b):
+                numOnes += int(b[(-1-i)])
+            flag = False
+
+            if numOnes % 2 == 1:
+                ans += '1'
+                numOnes -= 1
+            else:
+                ans += '0'
+            
+            if numOnes == 2:
+                flag = True
+            
+            i += 1
+        
+        if flag:
+            ans += '1'
+        
+
+        return ans[::-1]
