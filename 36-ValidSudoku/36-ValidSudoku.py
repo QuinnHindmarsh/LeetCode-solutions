9728d160b6ab9e20e2d2c6928ff21289be0577ec
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-22 19:28:32 +1030

    Update file 22/01/2025, 19:28:32

--- 36-ValidSudoku/36-ValidSudoku.py
+++ 36-ValidSudoku/36-ValidSudoku.py
@@ -1,16 +1,13 @@
-from random import randint
 class Solution:
     def isValidSudoku(self, board: List[List[str]]) -> bool:
         rows = [set() for i in range(len(board))]
         cols = [set() for i in range(len(board))]
         squares = [[set() for i in range(3)] for j in range(3)]
-        c = 0
 
         for i in range(len(board)):
             for j in range(len(board)):
                 ij = board[i][j]
                 if ij != '.':
-                    c += 1
                     if ij in rows[i]:
                         return False
                     if ij in cols[j]:
@@ -21,8 +18,5 @@ class Solution:
                     squares[i//3][j//3].add(ij)
                     cols[j].add(ij)
                     rows[i].add(ij)
-        if board == [["5","3",".",".","7",".",".",".","."],["6",".",".","1","9","5",".",".","."],[".","9","8",".",".",".",".","6","."],["8",".",".",".","6",".",".",".","3"],["4",".",".","8",".","3",".",".","1"],["7",".",".",".","2",".",".",".","6"],[".","6",".",".",".",".","2","8","."],[".",".",".","4","1","9",".",".","5"],[".",".",".",".","8",".",".","7","9"]]:
-            return True
 
-        print(c)
-        return True if c < 30 else False
+        return True
