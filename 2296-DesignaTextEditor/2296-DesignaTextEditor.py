e402bf5911832759928cb5ee71f56584dbc03271
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-22 22:19:49 +1030

    Update file 22/01/2025, 22:19:48

--- 2296-DesignaTextEditor/2296-DesignaTextEditor.py
+++ 2296-DesignaTextEditor/2296-DesignaTextEditor.py
@@ -1,26 +1,25 @@
 class TextEditor:
 
     def __init__(self):
-        self.s = ''
-        self.cursor = 0
+        # Two stacks that are relative to the cursor, r is in reverse
+        self.l, self.r = [], []
 
     def addText(self, text: str) -> None:
-        self.s = self.s[:self.cursor] + text + self.s[self.cursor:]
-        self.cursor += len(text)
+        self.l.extend(list(text))
 
     def deleteText(self, k: int) -> int:
-        new_cursor = max(0, self.cursor - k)
-        noOfChars = k if self.cursor - k >= 0 else self.cursor
-        self.s = self.s[:new_cursor] + self.s[self.cursor:]
-        self.cursor = new_cursor
-        return noOfChars
+        for _ in range((d := min(k, len(self.l)))):
+            self.l.pop()
+        return d
 
     def cursorLeft(self, k: int) -> str:
-        self.cursor = max(0, self.cursor - k)
-        start = max(0, self.cursor-10)
-        return self.s[start:self.cursor]
+        for _ in range(min(k, len(self.l))):
+            self.r.append(self.l.pop())
+        return ''.join(self.l[-10:])
 
     def cursorRight(self, k: int) -> str:
-        self.cursor = min(len(self.s), self.cursor + k)
-        start = max(0, self.cursor - 10)
-        return self.s[start:self.cursor]
+        for _ in range(min(k, len(self.r))):
+            self.l.append(self.r.pop())
+        return ''.join(self.l[-10:])
+        
+
