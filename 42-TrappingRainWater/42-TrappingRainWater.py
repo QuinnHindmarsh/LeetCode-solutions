958209c91588025675f971e2da90854c9037c6bc
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-30 10:21:11 +1030

    Update file 30/01/2025, 10:21:11

--- 42-TrappingRainWater/42-TrappingRainWater.py
+++ 42-TrappingRainWater/42-TrappingRainWater.py
@@ -1,22 +1,19 @@
 class Solution:
     def trap(self, h: List[int]) -> int:
         n = len(h)
-        maxl = 0
-        maxr = 0
-        lh = []
-        arr = [0] * n
+        l, r = 0, n - 1
+        maxL = h[l]
+        maxR = h[r]
         ans = 0
-        
 
-        for i in range(0,n):
-            lh.append(maxl)
-            maxl = max(maxl, h[i])
-
-        for i in range(n-1,-1,-1):
-            arr[i] = min(lh[i],maxr)
-            maxr = max(maxr, h[i])
-
-        for i in range(n):
-            ans += max(arr[i] - h[i], 0)
+        while l <= r:
+            if maxL < maxR:
+                ans += max(maxL - h[l],0)
+                maxL = max(maxL, h[l])
+                l += 1
+            else:
+                ans += max(maxR - h[r],0)
+                maxR = max(maxR, h[r])
+                r -= 1
         
         return ans
