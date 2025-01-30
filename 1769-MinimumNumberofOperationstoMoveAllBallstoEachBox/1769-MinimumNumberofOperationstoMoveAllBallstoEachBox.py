91daf90d5b4363775e3244dfb540fd329e464066
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-01-31 09:41:15 +1030

    Update file 31/01/2025, 09:41:15

--- 1769-MinimumNumberofOperationstoMoveAllBallstoEachBox/1769-MinimumNumberofOperationstoMoveAllBallstoEachBox.py
+++ 1769-MinimumNumberofOperationstoMoveAllBallstoEachBox/1769-MinimumNumberofOperationstoMoveAllBallstoEachBox.py
@@ -2,11 +2,23 @@ class Solution:
     def minOperations(self, boxes: str) -> List[int]:
         ans = []
         n = len(boxes)
+        pleft = [0] * n 
+        pright = [0] * n
+        
+        balls = 0
+        for i in range(n):
+            pleft[i] = pleft[i-1] + balls
+            if boxes[i] == '1':
+                balls += 1
+        
+        balls = 0 if boxes[-1] == '0' else 1
+        for i in range(n-2,-1,-1):
+            pright[i] = pright[i+1] + balls
+            if boxes[i] == '1':
+                balls += 1
 
         for i in range(n):
-            cur = 0
-            for j in range(n):
-                if boxes[j] == '1':
-                    cur += abs(i-j)
-            ans.append(cur)
+            ans.append(pleft[i] + pright[i])
+
+
         return ans
