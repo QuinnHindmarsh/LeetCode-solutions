635cd83e773a3f7685963282c55e0879becba63f
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-05 15:18:34 +1030

    Update file 05/02/2025, 15:18:34

--- 2469-ConverttheTemperature/2469-ConverttheTemperature.java
+++ 2469-ConverttheTemperature/2469-ConverttheTemperature.java
@@ -1,7 +1,5 @@
 class Solution {
     public double[] convertTemperature(double c) {
-        double[] ans = new double[] {c + 273.15, c * 1.80 + 32.00};
-
-        return ans;
+        return new double[] {c + 273.15, c * 1.80 + 32.00};
     }
 }
