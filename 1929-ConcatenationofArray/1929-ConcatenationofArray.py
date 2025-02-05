2bb73528975eb0fa76f2c6ac6255d87d6ec6a604
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-05 14:43:41 +1030

    Update file 05/02/2025, 14:43:41

--- 1929-ConcatenationofArray/1929-ConcatenationofArray.py
+++ 1929-ConcatenationofArray/1929-ConcatenationofArray.py
@@ -1,10 +1,10 @@
 class Solution:
     def getConcatenation(self, nums: List[int]) -> List[int]:
-        i = 0
-        n = len(nums)
+        # i = 0
+        # n = len(nums)
 
-        while i < n:
-            nums.append(nums[i])
-            i += 1
+        # while i < n:
+        #     nums.append(nums[i])
+        #     i += 1
 
-        return nums
+        return nums + nums
