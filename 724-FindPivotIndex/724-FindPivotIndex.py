a5fc012052efe2dbf819b9756c3a0313ed74478c
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-09 18:43:57 +1030

    Create file

---
+++ 724-FindPivotIndex/724-FindPivotIndex.py
@@ -0,0 +1,14 @@
+class Solution:
+    def pivotIndex(self, nums: List[int]) -> int:
+        rsum = sum(nums)
+        lsum = 0
+
+        for i in range(len(nums)):
+            rsum -= nums[i]
+
+            if lsum == rsum:
+                return i 
+            
+            lsum += nums[i]
+
+        return -1
