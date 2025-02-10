e7268cdddf95a2bf4f3538440d61c4a30e6b1465
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-10 17:26:47 +1030

    File updated at 10/02/2025, 17:26:47

--- 1672-RichestCustomerWealth/1672-RichestCustomerWealth.java
+++ 1672-RichestCustomerWealth/1672-RichestCustomerWealth.java
@@ -2,10 +2,7 @@ class Solution {
     public int maximumWealth(int[][] accounts) {
         int max = 0;
         for (int i = 0; i < accounts.length; i++){
-            if (sumArr(accounts[i]) > max){
-                max = sumArr(accounts[i]);
-            }
-
+            max = Math.max(sumArr(accounts[i]), max);
         }
         return max;
     }
