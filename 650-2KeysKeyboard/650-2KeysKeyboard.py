d693a531b2cd5adba3d2dab58cc5c16acc92e6ec
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-12 11:11:34 +1030

    File updated at 12/02/2025, 11:11:34

--- 650-2KeysKeyboard/650-2KeysKeyboard.py
+++ 650-2KeysKeyboard/650-2KeysKeyboard.py
@@ -1,41 +1,11 @@
 class Solution:
     def minSteps(self, n: int) -> int:
-        ans = 0
+        x = 0
+        p = 2
+        while p**2 <= n:
+            while n % p == 0:
+                x += p
+                n //= p
+            p += 1
 
-        # Work my way backwards
-
-        # Clip board size - recursive?
-        # 3 - 1 char - 3 moves
-        # 7 - 7 - 1 char
-        # 10 - 5 char - 7 moves
-        # 24 
-
-        # Even, half until it hits 0, each time you half add 2 moves 
-        # odd, 
-
-        # 5 10 15
-        # 5 7  8 
-        
-        # Odd, greatest divsor repeat untill you hit 0
-
-        # Greatist proper devider
-        def GPD(num):
-            for i in range(2,isqrt(num)+1):
-                if num % i == 0:
-                    return num // i
-            return 1
-
-        def steps(num):
-            if num == 1: return 0
-            if num == 2: return 2
-
-            if num % 2 == 0:
-                return 2 + steps(num//2)
-
-            gpd = GPD(num)
-            print(gpd)
-            if gpd == 1:
-                return num 
-            return (num // gpd) + steps(gpd)
-        
-        return steps(n)
+        return x + n if n > 1 else x
