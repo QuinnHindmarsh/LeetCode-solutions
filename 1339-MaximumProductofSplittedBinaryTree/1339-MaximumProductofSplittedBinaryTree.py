32ca34ac6b7979751cee583bea2eddbce4eecf81
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-02-17 11:30:35 +1030

    File updated at 17/02/2025, 11:30:34

--- 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.py
+++ 1339-MaximumProductofSplittedBinaryTree/1339-MaximumProductofSplittedBinaryTree.py
@@ -1,43 +1,24 @@
-# Definition for a binary tree node.
-# class TreeNode:
-#     def __init__(self, val=0, left=None, right=None):
-#         self.val = val
-#         self.left = left
-#         self.right = right
-class Solution:
-    def maxProduct(self, root: Optional[TreeNode]) -> int:
-        subtreeVals = {}
+        return root.val+total(root.left)+total(root.right)
 
-        def calc_subtree_vals(node):
-            if node in subtreeVals:
-                return subtreeVals[node]
+    sum1=total(root)
 
-            lsum = 0
-            rsum = 0
-            csum = node.val
+    def traversal(root):
+        nonlocal maximum
+        if root==None:
+            return 0
 
-            if node.left:
-                lsum = calc_subtree_vals(node.left)
-            if node.right: 
-                rsum = calc_subtree_vals(node.right)
+        ls=traversal(root.left)
+        rs=traversal(root.right)
 
-            subtreeVals[node] = lsum + rsum + csum
-            return subtreeVals[node]
-
-
-        def max_traversal(node):
-            c = subtreeVals[node] * (total - subtreeVals[node])
-            l = 0
-            r = 0 
-
-            if node.left:
-                l = max_traversal(node.left)
-            if node.right:
-                r = max_traversal(node.right)
-
-            return max(c, l, r) 
+        sumz=ls+rs+root.val
         
-        calc_subtree_vals(root)
-        total = subtreeVals[root] 
+        if (sum1-rs)*rs>maximum:
+            maximum=(sum1-rs)*rs
+        if (sum1-ls)*ls>maximum:
+            maximum=(sum1-ls)*ls
 
-        return max_traversal(root) % 1000000007
+        return sumz
+        
+    maximum=-99999
+    traversal(root)
+    return maximum%(10**9+7)
