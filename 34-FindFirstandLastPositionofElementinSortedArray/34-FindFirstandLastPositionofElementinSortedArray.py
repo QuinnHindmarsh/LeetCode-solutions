bc2d74ebd0c31812dd9ebb17f70c2346064e0146
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-05-16 15:29:37 +0930

    File updated at 16/05/2025, 15:29:37

--- 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
+++ 34-FindFirstandLastPositionofElementinSortedArray/34-FindFirstandLastPositionofElementinSortedArray.py
@@ -1,40 +1,40 @@
+# Last updated: 16/05/2025, 15:29:37
 class Solution:
     def searchRange(self, nums: List[int], target: int) -> List[int]:
-        def findLeft(arr,tar,l,r):
-            l, r = 0, len(arr) -1
+        
+        def lower_bound(nums, tar):
+            l = 0 
+            r = len(nums) -1 
+
             while l < r:
-                mid = (l + r) // 2 
-                if arr[mid] > tar:
+                mid = (l+r) // 2
+
+                if nums[mid] > tar:
                     r = mid -1
-                elif arr[mid] < tar:
+                elif nums[mid] < tar:
                     l = mid +1
                 else:
                     r = mid
 
-            return l if l < len(arr) and arr[l] == tar else -1
+            return l if nums[l] == target else -1
 
-        
-        def findRight(arr,tar,l,r):
-            l, r = 0, len(arr) -1
+        def upper_bound(nums,tar):
+            l = 0
+            r = len(nums) -1
 
-            while l < r:
-                mid = (l + r) // 2 + 1
-                if arr[mid] > tar:
-                    r = mid -1
-                elif arr[mid] < tar:
+            while l < r: 
+                mid = (l + r+1) // 2
+
+                if nums[mid] > tar:
+                    r = mid -1 
+                elif nums[mid] < target:
                     l = mid +1
                 else:
                     l = mid
-            return r if r < len(arr) and arr[r] == tar else -1
 
-        if len(nums) == 0:
+            return r if nums[r] == target else -1
+
+        if not nums:
             return [-1,-1]
 
-        ans = [-1,-1]
-        l = findLeft(nums,target,0,len(nums) -1)
-        r = findRight(nums, target, 0, len(nums)-1)
-        ans[0] = l 
-        ans[1] = r 
-
-        return ans
-
+        return [lower_bound(nums, target), upper_bound(nums, target)]
