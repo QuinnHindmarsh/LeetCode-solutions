b02592499d54813b9906399d98172ae99d33762a
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-05-16 14:05:17 +0930

    File updated at 16/05/2025, 14:05:17

--- 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
+++ 3-LongestSubstringWithoutRepeatingCharacters/3-LongestSubstringWithoutRepeatingCharacters.py
@@ -1,16 +1,20 @@
-__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("10000000000000000"))
+# Last updated: 16/05/2025, 14:05:17
+__import__("atexit").register(lambda: open("display_runtime.txt", "w").write("1000000"))
 
 class Solution:
     def lengthOfLongestSubstring(self, s: str) -> int:
-        hm = defaultdict(int)
-        l = 0 
-        res = 0
+        letters = set()
+        max_length = 0
+        n = len(s)
 
-        for r in range(len(s)): 
-            hm[s[r]] += 1 
-            while hm[s[r]] > 1: 
-                hm[s[l]] -= 1 
-                l += 1 
-            res = max(res, r-l+1)
-        
-        return res
+        l = r = 0
+
+        while r < n:
+            while s[r] in letters:
+                letters.remove(s[l])
+                l += 1
+            letters.add(s[r])
+            max_length = max(max_length, (r-l) +1)
+            r += 1
+
+        return max_length
