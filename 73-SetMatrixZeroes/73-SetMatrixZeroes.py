f3760711a12e3fc223d925a49b0cba3b46ed98b9
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-05-16 11:49:25 +0930

    File updated at 16/05/2025, 11:49:25

--- 73-SetMatrixZeroes/73-SetMatrixZeroes.py
+++ 73-SetMatrixZeroes/73-SetMatrixZeroes.py
@@ -1,22 +1,40 @@
+# Last updated: 16/05/2025, 11:49:25
 class Solution:
     def setZeroes(self, matrix: List[List[int]]) -> None:
         """
         Do not return anything, modify matrix in-place instead.
         """
-        rows = set()
-        cols = set()
+        first_row_has_zero = first_col_has_zero = False
 
-        m = len(matrix)
-        n = len(matrix[0])
+        for i in range(len(matrix)):
+            if matrix[i][0] == 0:
+                first_col_has_zero = True
+                break
 
-        for i in range(m):
-            for j in range(n):
+        for j in range(len(matrix[0])):
+            if matrix[0][j] == 0:
+                first_row_has_zero = True
+                break
+
+                
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[i])):
                 if matrix[i][j] == 0:
-                    rows.add(i)
-                    cols.add(j)
+                    matrix[0][j] = 0
+                    matrix[i][0] = 0
 
-        for i in range(m):
-            for j in range(n):  
-                if i in rows or j in cols:
+        for i in range(1,len(matrix)):
+            for j in range(1,len(matrix[i])):
+                if matrix[i][0] == 0 or matrix[0][j] == 0:
                     matrix[i][j] = 0
 
+        for i in range(len(matrix)):
+            if first_col_has_zero:
+                matrix[i][0] = 0
+
+        for j in range(len(matrix[0])):
+            if first_row_has_zero:
+                matrix[0][j] = 0
+
+            
+                    
