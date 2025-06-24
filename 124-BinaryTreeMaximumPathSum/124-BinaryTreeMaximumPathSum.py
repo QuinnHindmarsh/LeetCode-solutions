cbd01febc4e7c6dddafa3da25c4bcc9ffbc88850
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-06-24 13:51:44 +0930

    File updated at 24/06/2025, 13:51:43

--- 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
+++ 124-BinaryTreeMaximumPathSum/124-BinaryTreeMaximumPathSum.py
@@ -1,4 +1,4 @@
-# Last updated: 24/06/2025, 13:49:39
+# Last updated: 24/06/2025, 13:51:43
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -8,26 +8,18 @@
 class Solution:
     def maxPathSum(self, root: Optional[TreeNode]) -> int:
         self.mx = -float('inf')
-        self.is_positive_node = False
-        self.max_node = -float('inf')
 
         self.mx = max(self.dfs(root), self.mx)
 
-        if not self.is_positive_node:
-            return self.max_node
+ 
         return self.mx
 
     def dfs(self, node):
         if not node:
             return 0 
         
-        if node.val > 0:
-            self.is_positive_node = True
+        left = max(self.dfs(node.left), 0)
+        right = max(self.dfs(node.right), 0)
 
-        self.max_node = max(self.max_node, node.val) 
-
-        left = self.dfs(node.left)
-        right = self.dfs(node.right)
-
-        self.mx = max(self.mx, left + right + node.val, node.val)
-        return max(max(left,right,0) + node.val, 0)
+        self.mx = max(self.mx, left + right + node.val)
+        return max(left,right) + node.val
