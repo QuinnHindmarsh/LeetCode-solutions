36235828997da940b24e028ea8769285a05a87c2
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-06-26 16:24:21 +0930

    File updated at 26/06/2025, 16:24:20

--- 78-Subsets/78-Subsets.py
+++ 78-Subsets/78-Subsets.py
@@ -1,13 +1,12 @@
-# Last updated: 26/06/2025, 16:23:02
+# Last updated: 26/06/2025, 16:24:20
 class Solution:
     def subsets(self, nums: List[int]) -> List[List[int]]:
-        ans = set()
+        ans = []
         state = []
 
         def dfs(i):
-            ans.add(tuple(state))
-
             if i == len(nums):
+                ans.append(state[:])
                 return
             
             
@@ -17,4 +16,4 @@ class Solution:
             dfs(i+1)
 
         dfs(0)
-        return list(ans)
+        return ans
