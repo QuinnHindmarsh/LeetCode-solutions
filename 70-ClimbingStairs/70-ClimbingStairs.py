170a6c2aef60b9f7a13ef3d59d0e335a3ad0748f
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-06-27 11:38:35 +0930

    File updated at 27/06/2025, 11:38:34

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,4 +1,4 @@
-# Last updated: 27/06/2025, 11:37:32
+# Last updated: 27/06/2025, 11:38:34
 class Solution:
     def climbStairs(self, n: int) -> int:
         # 1 indexed
@@ -10,9 +10,12 @@ class Solution:
             
             if i == 2:
                 return 2 
+
+            if i in dp:
+                return dp[i]
             
-            fib1 = dp[i-1] if i-1 in dp else fib(i-1)
-            fib2 = dp[i-2] if i-2 in dp else fib(i-2)
+            fib1 = fib(i-1)
+            fib2 =  fib(i-2)
 
             dp[i] = fib1 + fib2
             return dp[i]
