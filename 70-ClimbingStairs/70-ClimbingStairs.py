36f55f44ba59b0624d044bcf0e642b123fb47b36
QuinnHindmarsh <qhindmarsh@gmail.com>
2025-06-27 11:37:33 +0930

    File updated at 27/06/2025, 11:37:32

--- 70-ClimbingStairs/70-ClimbingStairs.py
+++ 70-ClimbingStairs/70-ClimbingStairs.py
@@ -1,18 +1,20 @@
+# Last updated: 27/06/2025, 11:37:32
 class Solution:
     def climbStairs(self, n: int) -> int:
-        if n == 1:
-            return 1
-        elif n == 2:
-            return 2
+        # 1 indexed
+        dp = {}
 
-        last = 1
-        cur = 2
-        i = 2
+        def fib(i):
+            if i == 1: 
+                return 1
+            
+            if i == 2:
+                return 2 
+            
+            fib1 = dp[i-1] if i-1 in dp else fib(i-1)
+            fib2 = dp[i-2] if i-2 in dp else fib(i-2)
 
-        while i < n:
-            temp = cur
-            cur += last
-            last = temp
-            i += 1
+            dp[i] = fib1 + fib2
+            return dp[i]
 
-        return cur
+        return fib(n)
