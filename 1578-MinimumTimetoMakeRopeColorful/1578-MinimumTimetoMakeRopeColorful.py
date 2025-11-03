a2039b80465a6220dc222474e140666a3d54290d
Quinn Hindmarsh <qhindmarsh@gmail.com>
2025-11-03 13:15:38 +1030

    Create 1578-MinimumTimetoMakeRopeColorful

---
+++ 1578-MinimumTimetoMakeRopeColorful/1578-MinimumTimetoMakeRopeColorful.py
@@ -0,0 +1,13 @@
+# Last updated: 03/11/2025, 13:15:38
+class Solution:
+    def minCost(self, colors: str, neededTime: List[int]) -> int:
+        ans = 0 
+
+        for i in range(1,len(colors)):
+            if colors[i] == colors[i-1]:
+                ans += min(neededTime[i],neededTime[i-1])
+                
+                neededTime[i] = max(neededTime[i],neededTime[i-1])
+
+
+        return ans
