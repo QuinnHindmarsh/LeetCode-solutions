c9ecb80cc6e6e914d3b586d9eb822d5b7aaf2153
Quinn Hindmarsh <qhindmarsh@gmail.com>
2025-12-07 09:47:42 +0800

    File updated at 07/12/2025, 09:47:45

--- 650-2KeysKeyboard/650-2KeysKeyboard.py
+++ 650-2KeysKeyboard/650-2KeysKeyboard.py
@@ -1,37 +1,38 @@
-class Solution:
-    def minSteps(self, n: int) -> int:
-        ans = 0
-
-        # Work my way backwards
-
-        # Clip board size - recursive?
-        # 3 - 1 char - 3 moves
-        # 7 - 7 - 1 char
-        # 10 - 5 char - 7 moves
-        # 24 
-
-        # Even, half until it hits 0, each time you half add 2 moves 
-        # odd, 
-
-        # 5 10 15
-        # 5 7  8 
-        
-        # Odd, greatest divsor repeat untill you hit 0
-
-        # Greatist proper devider
-        def GPD(num):
-            for i in range(2,isqrt(num)+1):
-                if num % i == 0:
-                    return num // i
-            return 1
-
-        def steps(num):
-            if num == 1: return 0
-            if num == 2: return 2
-
-            gpd = GPD(num)
-            if gpd == 1:
-                return num 
-            return (num // gpd) + steps(gpd)
-        
-        return steps(n)
+# Last updated: 07/12/2025, 09:47:45
+1class Solution:
+2    def minSteps(self, n: int) -> int:
+3        ans = 0
+4
+5        # Work my way backwards
+6
+7        # Clip board size - recursive?
+8        # 3 - 1 char - 3 moves
+9        # 7 - 7 - 1 char
+10        # 10 - 5 char - 7 moves
+11        # 24 
+12
+13        # Even, half until it hits 0, each time you half add 2 moves 
+14        # odd, 
+15
+16        # 5 10 15
+17        # 5 7  8 
+18        
+19        # Odd, greatest divsor repeat untill you hit 0
+20
+21        # Greatist proper devider
+22        def GPD(num):
+23            for i in range(2,isqrt(num)+1):
+24                if num % i == 0:
+25                    return num // i
+26            return 1
+27
+28        def steps(num):
+29            if num == 1: return 0
+30            if num == 2: return 2
+31
+32            gpd = GPD(num)
+33            if gpd == 1:
+34                return num 
+35            return (num // gpd) + steps(gpd)
+36        
+37        return steps(n)
