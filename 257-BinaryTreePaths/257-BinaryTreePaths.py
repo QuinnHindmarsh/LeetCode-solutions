fcb9c6bd13bf72f4d3c194d19e22ea3068928f06
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-01-05 15:26:47 +0800

    File updated at 05/01/2026, 15:26:48

--- 257-BinaryTreePaths/257-BinaryTreePaths.py
+++ 257-BinaryTreePaths/257-BinaryTreePaths.py
@@ -1,17 +1,22 @@
-# Last updated: 05/01/2026, 15:13:03
+# Last updated: 05/01/2026, 15:26:48
 1class Solution:
-2    def getRow(self, rowIndex: int) -> List[int]:
-3        cur = [1]
-4
-5
-6        for _ in range(1,rowIndex+1):
-7            last = cur
-8            cur = []
-9
-10            for i in range(len(last)+1):
-11                if i == 0 or i == len(last):
-12                    cur.append(1)
-13                else:
-14                    cur.append(last[i-1] + last[i])
+2    def luckyNumbers(self, matrix: List[List[int]]) -> List[int]:
+3
+4        r = len(matrix)
+5        c = len(matrix[0])
+6        r_min = [float('inf')] * r
+7        c_max = [-float('inf')] * c
+8        ans = []
+9        
+10        for i in range(c):
+11            for j in range(r):
+12                r_min[j] = min(r_min[j], matrix[j][i])
+13                c_max[i] = max(c_max[i], matrix[j][i])
+14        
 15
-16        return cur
+16        for i in range(c):
+17            for j in range(r):
+18                if matrix[j][i] == c_max[i] == r_min[j]:
+19                    ans.append(matrix[j][i])
+20
+21        return ans
