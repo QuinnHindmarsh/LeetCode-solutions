7c6fad668e3404819071409b1336a13683a3b63b
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-01-05 15:13:03 +0800

    File updated at 05/01/2026, 15:13:03

--- 257-BinaryTreePaths/257-BinaryTreePaths.py
+++ 257-BinaryTreePaths/257-BinaryTreePaths.py
@@ -1,27 +1,17 @@
-# Last updated: 05/01/2026, 15:05:42
-1# Definition for a binary tree node.
-2# class TreeNode:
-3#     def __init__(self, val=0, left=None, right=None):
-4#         self.val = val
-5#         self.left = left
-6#         self.right = right
-7class Solution:
-8    def binaryTreePaths(self, root: Optional[TreeNode]) -> List[str]:
-9        ans = []
-10        path = []
-11        def dfs(node):
-12            if not node:
-13                return
-14            
-15            path.append(str(node.val))
-16            dfs(node.left)
-17            dfs(node.right)
-18            
-19            if not node.left and not node.right:
-20                ans.append("->".join(path))
-21            path.pop()
-22
-23
-24
-25        dfs(root)
-26        return ans
+# Last updated: 05/01/2026, 15:13:03
+1class Solution:
+2    def getRow(self, rowIndex: int) -> List[int]:
+3        cur = [1]
+4
+5
+6        for _ in range(1,rowIndex+1):
+7            last = cur
+8            cur = []
+9
+10            for i in range(len(last)+1):
+11                if i == 0 or i == len(last):
+12                    cur.append(1)
+13                else:
+14                    cur.append(last[i-1] + last[i])
+15
+16        return cur
