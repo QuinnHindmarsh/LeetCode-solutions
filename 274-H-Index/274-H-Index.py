6a040a8818ec1f5a790eb1f2eae8f7b7cd46742b
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-03-11 14:42:09 +1030

    File updated at 11/03/2026, 14:42:11

--- 274-H-Index/274-H-Index.py
+++ 274-H-Index/274-H-Index.py
@@ -1,11 +1,16 @@
-# Last updated: 11/03/2026, 14:31:27
+# Last updated: 11/03/2026, 14:42:11
 1class Solution:
-2    def hIndex(self, citations: List[int]) -> int:
-3        citations.sort()
-4        n = len(citations)
+2    def taskSchedulerII(self, tasks: List[int], space: int) -> int:
+3        last_done = {}
+4        days = 0 
 5
-6        for i in range(n):
-7            if citations[i] >= n - i:
-8                return n - i
-9
-10        return 0
+6        for i in range(len(tasks)):
+7            typ = tasks[i]
+8            if typ not in last_done or days - last_done[typ] >= space: 
+9                days += 1
+10                last_done[typ] = days
+11                continue
+12            next_day = last_done[typ] + space +1
+13            days += next_day - days
+14            last_done[typ] = days
+15        return days
