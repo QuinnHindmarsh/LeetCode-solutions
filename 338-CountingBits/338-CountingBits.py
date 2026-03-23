47d70322a4a74bb5f31d5bd1c5ef2299acc7f35d
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-03-23 21:21:22 +1030

    File updated at 23/03/2026, 21:21:22

--- 338-CountingBits/338-CountingBits.py
+++ 338-CountingBits/338-CountingBits.py
@@ -1,10 +1,4 @@
-# Last updated: 23/03/2026, 21:20:30
+# Last updated: 23/03/2026, 21:21:22
 1class Solution:
 2    def countBits(self, n: int) -> List[int]:
-3        ans = []
-4
-5
-6        for i in range(n+1): 
-7            ans.append(bin(i).count('1'))
-8
-9        return ans
+3        return [bin(i).count('1') for i in range(n+1)]
