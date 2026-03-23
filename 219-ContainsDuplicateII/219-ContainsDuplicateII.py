8e1ef7d05bebe8740a2b3d39bd857e517b732416
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-03-23 20:49:47 +1030

    File updated at 23/03/2026, 20:49:47

--- 219-ContainsDuplicateII/219-ContainsDuplicateII.py
+++ 219-ContainsDuplicateII/219-ContainsDuplicateII.py
@@ -1,16 +1,15 @@
-class Solution:
-    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
-        locs = {}
-
-        for i in range(len(nums)):
-            if nums[i] in locs:
-                locs[nums[i]].append(i)
-            else:
-                locs[nums[i]] = [i]
-        
-        for key in locs:
-            for i in range(len(locs[key])):
-                for j in range(i+1,len(locs[key])):
-                    if abs(locs[key][i] - locs[key][j]) <= k:
-                        return True
-        return False
+# Last updated: 23/03/2026, 20:49:47
+1class Solution:
+2    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+3        last = {}
+4
+5
+6        for i,num in enumerate(nums): 
+7            if num in last: 
+8                if abs(last[num] - i )<= k: 
+9                    return True
+10
+11            last[num] = i 
+12
+13
+14        return False
