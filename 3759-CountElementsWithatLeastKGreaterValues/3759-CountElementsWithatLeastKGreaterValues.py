6faa7264a2a125da0d6dac7166f5e3dbf98a2ea6
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-04-02 12:31:12 +1030

    File updated at 02/04/2026, 12:31:12

--- 3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.py
+++ 3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.py
@@ -1,24 +1,8 @@
-# Last updated: 02/04/2026, 11:57:33
+# Last updated: 02/04/2026, 12:31:12
 1class Solution:
-2    def countElements(self, nums: List[int], k: int) -> int:
-3        greater = 0 
-4        last_number = None 
-5        count_of_last = 0 
-6        ans = 0 
-7
-8        heap = nums 
-9        heapify_max(heap)
-10
-11        while heap:
-12            num = heappop_max(heap) 
-13            if num == last_number: 
-14                count_of_last += 1
-15            else:
-16                greater += count_of_last
-17                count_of_last = 1
-18                last_number = num
-19
-20            if greater >= k: 
-21                ans += 1
-22
-23        return ans
+2    def stoneGameIX(self, stones: List[int]) -> bool:
+3        freq = defaultdict(int)
+4        for x in stones: freq[x % 3] += 1
+5        
+6        if freq[0]%2 == 0: return bool(freq[1] and freq[2])
+7        return abs(freq[1] - freq[2]) >= 3
