8f024372ebaf2a35b451d76a037947ed85c3e0cc
Quinn Hindmarsh <qhindmarsh@gmail.com>
2026-04-02 12:41:36 +1030

    File updated at 02/04/2026, 12:41:36

--- 3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.py
+++ 3759-CountElementsWithatLeastKGreaterValues/3759-CountElementsWithatLeastKGreaterValues.py
@@ -1,8 +1,14 @@
-# Last updated: 02/04/2026, 12:31:12
+# Last updated: 02/04/2026, 12:41:36
 1class Solution:
-2    def stoneGameIX(self, stones: List[int]) -> bool:
-3        freq = defaultdict(int)
-4        for x in stones: freq[x % 3] += 1
-5        
-6        if freq[0]%2 == 0: return bool(freq[1] and freq[2])
-7        return abs(freq[1] - freq[2]) >= 3
+2    def myPow(self, x: float, n: int) -> float:
+3        if n < 0:
+4            x = 1 / x
+5            n = -n
+6        
+7        result = 1
+8        while n > 0:
+9            if n % 2 == 1:    
+10                result *= x
+11            x *= x             
+12            n //= 2            
+13        return result
